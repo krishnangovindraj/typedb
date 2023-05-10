b2bf6f8c7977df7775be68edfa0ab8fce03a41ef
Krishnan Govindraj <krishnan@vaticle.com>
2023-05-10 10:37:24 +0100

    Randomise vertex insertion ; Licenses

--- test/benchmark/database/DataSource.java
+++ test/benchmark/database/DataSource.java
@@ -1,10 +1,32 @@
+/*
+ * Copyright (C) 2022 Vaticle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.vaticle.typedb.core.benchmark.database;
 
 import com.vaticle.typedb.common.collection.Pair;
 import com.vaticle.typedb.core.common.exception.TypeDBException;
 import com.vaticle.typedb.core.common.iterator.AbstractFunctionalIterator;
 import com.vaticle.typedb.core.common.iterator.FunctionalIterator;
+import com.vaticle.typedb.core.common.iterator.Iterators;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import static com.vaticle.typedb.core.common.exception.ErrorMessage.Internal.UNIMPLEMENTED;
@@ -24,7 +46,12 @@ public class DataSource {
     }
 
     FunctionalIterator<Long> vertices() {
-        return new VertexGenerator();
+        List<Long> shuffled = new ArrayList<>();
+        for (int i = 0; i < nVertices; i++) {
+            shuffled.add(Long.valueOf(i));
+        }
+        Collections.shuffle(shuffled);
+        return Iterators.iterate(shuffled);
     }
 
     FunctionalIterator<Pair<Long, Long>> edges() {
