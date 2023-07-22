748d0cdabd3ef8f248db76c033f00117e49ab3a6
Krishnan Govindraj <krishnan@vaticle.com>
2023-07-23 00:20:00 +0100

    Rip out a copy of ReasonerProducer

---
+++ reasoner/v4/ReasonerConsumerV4.java
@@ -0,0 +1,7 @@
+package com.vaticle.typedb.core.reasoner.v4;
+
+import com.vaticle.typedb.core.concept.answer.ConceptMap;
+import com.vaticle.typedb.core.reasoner.ReasonerConsumer;
+
+public interface ReasonerConsumerV4 extends ReasonerConsumer<ConceptMap> {
+}
