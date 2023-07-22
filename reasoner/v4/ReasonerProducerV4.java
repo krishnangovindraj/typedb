d537a76341706ef269ce2d454367e9d7635d8105
Krishnan Govindraj <krishnan@vaticle.com>
2023-07-23 00:20:00 +0100

    Rip out a copy of ReasonerProducer

---
+++ reasoner/v4/ReasonerProducerV4.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2022 Vaticle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.vaticle.typedb.core.reasoner.v4;
+
+import com.vaticle.typedb.core.common.parameters.Options;
+import com.vaticle.typedb.core.concept.answer.ConceptMap;
+import com.vaticle.typedb.core.concurrent.actor.Actor;
+import com.vaticle.typedb.core.concurrent.producer.Producer;
+import com.vaticle.typedb.core.logic.resolvable.Concludable;
+import com.vaticle.typedb.core.logic.resolvable.ResolvableConjunction;
+import com.vaticle.typedb.core.logic.resolvable.ResolvableDisjunction;
+import com.vaticle.typedb.core.reasoner.ExplainablesManager;
+import com.vaticle.typedb.core.reasoner.ReasonerConsumer;
+import com.vaticle.typedb.core.reasoner.answer.Explanation;
+import com.vaticle.typedb.core.reasoner.controller.ControllerRegistry;
+import com.vaticle.typedb.core.reasoner.processor.AbstractProcessor;
+import com.vaticle.typedb.core.traversal.common.Modifiers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.vaticle.typedb.core.common.exception.ErrorMessage.Internal.ILLEGAL_STATE;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.EXCEPTION;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.FINISHED;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.INIT;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.INITIALISING;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.PULLING;
+import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.READY;
+
+
+@ThreadSafe
+public abstract class ReasonerProducerV4<ANSWER> implements Producer<ANSWER>, ReasonerConsumerV4 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReasonerProducerV4.class);
+
+    private final ReasonerCoordinator reasonerCoordinator;
+    private final ExplainablesManager explainablesManager;
+    final AtomicInteger requiredAnswers;
+    final Options.Query options;
+    private Throwable exception;
+    Queue<ANSWER> queue;
+    State state;
+
+    enum State {
+        INIT,
+        INITIALISING,
+        READY,
+        PULLING,
+        FINISHED,
+        EXCEPTION
+    }
+
+    // TODO: this class should not be a Producer, it implements a different async processing mechanism
+    private ReasonerProducerV4(Options.Query options, ReasonerCoordinator reasonerCoordinator, ExplainablesManager explainablesManager) {
+        this.options = options;
+        this.reasonerCoordinator = reasonerCoordinator;
+        this.explainablesManager = explainablesManager;
+        this.queue = null;
+        this.requiredAnswers = new AtomicInteger();
+        this.state = INIT;
+    }
+
+    ControllerRegistry ReasonerCoordinator() {
+        return reasonerCoordinator;
+    }
+
+    @Override
+    public synchronized void produce(Queue<ANSWER> queue, int requestedAnswers, Executor executor) {
+        assert this.queue == null || this.queue == queue;
+        assert requestedAnswers > 0;
+        if (state == EXCEPTION) queue.done(exception);
+        else if (state == FINISHED) queue.done();
+        else {
+            this.queue = queue;
+            requiredAnswers.addAndGet(requestedAnswers);
+            if (state == INIT) initialise();
+            else if (state == READY) pull();
+        }
+    }
+
+    private void initialise() {
+        assert state == INIT;
+        state = INITIALISING;
+        initialiseRoot();
+    }
+
+    abstract void initialiseRoot();
+
+    synchronized void pull() {
+        assert state == READY;
+        state = PULLING;
+        rootSubgoal.execute(actor -> actor.rootPull());
+    }
+
+    @Override
+    public synchronized void finish() {
+        // note: root resolver calls this single-threaded, so is thread safe
+        LOG.trace("All answers found.");
+        if (state != FINISHED && state != EXCEPTION) {
+            if (queue == null) {
+                assert state != PULLING;
+                assert requiredAnswers.get() == 0;
+            } else {
+                requiredAnswers.set(0);
+                queue.done();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void exception(Throwable e) {
+        if (state != FINISHED && state != EXCEPTION) {
+            exception = e;
+            if (queue == null) {
+                assert state != PULLING;
+                assert requiredAnswers.get() == 0;
+            } else {
+                requiredAnswers.set(0);
+                queue.done(e.getCause());
+            }
+        }
+    }
+
+    @Override
+    public synchronized void receiveAnswer(ConceptMap answer) {
+        state = READY;
+        queue.put(transformAnswer(answer));
+        if (requiredAnswers.decrementAndGet() > 0) pull();
+    }
+
+    protected abstract ANSWER transformAnswer(ConceptMap answer);
+
+    @Override
+    public void recycle() {
+
+    }
+
+
+    @Override
+    public void setRootProcessor(Actor.Driver<? extends AbstractProcessor<?, ConceptMap, ?, ?>> rootProcessor) {
+        throw new RuntimeException(ILLEGAL_STATE.message());
+    }
+
+    public static class Basic extends ReasonerProducerV4<ConceptMap> {
+
+        Basic(Options.Query options, ReasonerCoordinator reasonerCoordinator, ExplainablesManager explainablesManager) {
+            super(options, reasonerCoordinator, explainablesManager);
+        }
+
+        @Override
+        void initialiseRoot() {
+            TODO
+        }
+
+        @Override
+        protected ConceptMap transformAnswer(ConceptMap answer) {
+            return answer;
+        }
+    }
+}
