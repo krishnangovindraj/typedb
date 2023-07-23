819c5268cabbeda7ee66565d0ba5ae7885134a1e
Krishnan Govindraj <krishnan@vaticle.com>
2023-07-23 13:38:11 +0100

    WIP: Small progress on ReasonerProducer.RootNode

--- reasoner/v4/ReasonerProducerV4.java
+++ reasoner/v4/ReasonerProducerV4.java
@@ -17,9 +17,12 @@
 
 package com.vaticle.typedb.core.reasoner.v4;
 
+import com.vaticle.typedb.core.common.exception.TypeDBException;
 import com.vaticle.typedb.core.common.parameters.Options;
 import com.vaticle.typedb.core.concept.answer.ConceptMap;
+import com.vaticle.typedb.core.concurrent.actor.Actor;
 import com.vaticle.typedb.core.concurrent.producer.Producer;
+import com.vaticle.typedb.core.logic.resolvable.ResolvableConjunction;
 import com.vaticle.typedb.core.reasoner.ExplainablesManager;
 import com.vaticle.typedb.core.reasoner.controller.ControllerRegistry;
 import org.slf4j.Logger;
@@ -28,7 +31,10 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
+import static com.vaticle.typedb.core.common.exception.ErrorMessage.Internal.ILLEGAL_STATE;
+import static com.vaticle.typedb.core.common.exception.ErrorMessage.Internal.UNIMPLEMENTED;
 import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.EXCEPTION;
 import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.FINISHED;
 import static com.vaticle.typedb.core.reasoner.v4.ReasonerProducerV4.State.INIT;
@@ -42,7 +48,7 @@ public abstract class ReasonerProducerV4<ANSWER> implements Producer<ANSWER>, Re
 
     private static final Logger LOG = LoggerFactory.getLogger(ReasonerProducerV4.class);
 
-    private final NodeRegistry nodeRegistry;
+    protected final NodeRegistry nodeRegistry;
     private final ExplainablesManager explainablesManager;
     final AtomicInteger requiredAnswers;
     final Options.Query options;
@@ -69,7 +75,7 @@ public abstract class ReasonerProducerV4<ANSWER> implements Producer<ANSWER>, Re
         this.state = INIT;
     }
 
-    ControllerRegistry ReasonerCoordinator() {
+    NodeRegistry nodeRegistry() {
         return nodeRegistry;
     }
 
@@ -146,18 +152,54 @@ public abstract class ReasonerProducerV4<ANSWER> implements Producer<ANSWER>, Re
 
     public static class Basic extends ReasonerProducerV4<ConceptMap> {
 
-        Basic(Options.Query options, NodeRegistry nodeRegistry, ExplainablesManager explainablesManager) {
+        private final ResolvableConjunction conjunction;
+        private Actor.Driver<RootNode> rootNode; // TODO: Make final, init in constructor, change return type of initialiseRoot
+
+        Basic(ResolvableConjunction conjunction, Options.Query options, NodeRegistry nodeRegistry, ExplainablesManager explainablesManager) {
             super(options, nodeRegistry, explainablesManager);
+            this.conjunction = conjunction;
         }
 
         @Override
         void initialiseRoot() {
-            TODO
+            rootNode = nodeRegistry.createDriver(nodeDriver -> new RootNode(nodeRegistry, nodeDriver));
         }
 
         @Override
         protected ConceptMap transformAnswer(ConceptMap answer) {
             return answer;
+        }
+
+        private class RootNode extends ActorNode<RootNode> {
+
+            private final NodeRegistry.ConjunctionSubRegistry subRegistry;
+
+            protected RootNode(NodeRegistry nodeRegistry, Driver<RootNode> driver) {
+                super(nodeRegistry, driver, () -> "RootNode: " + conjunction.pattern());
+                this.subRegistry = nodeRegistry.conjunctionSubRegistry(conjunction);
+            }
+
+            @Override
+            protected void exception(Throwable e) {
+                throw TypeDBException.of(UNIMPLEMENTED);
+            }
+
+            @Override
+            public void readAnswerAt(ActorNode<?> sender, int index) {
+                subRegistry.getNode(new ConceptMap()).readAnswerAt(this, index);
+            }
+
+            @Override
+            public void receive(ActorNode<?> sender, Message message) {
+                switch (message.type()) {
+                    case ANSWER:
+                        Basic.this.receiveAnswer(message.answer().get());
+                    case DONE:
+                        Basic.this.finish();
+                    default:
+                        throw TypeDBException.of(ILLEGAL_STATE);
+                }
+            }
         }
     }
 }
