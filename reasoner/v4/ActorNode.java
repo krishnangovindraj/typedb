0d036d8a29e14126b1acb374a52635734a0a8b76
Krishnan Govindraj <krishnan@vaticle.com>
2023-07-24 22:26:18 +0100

    Port everything to ports

--- reasoner/v4/ActorNode.java
+++ reasoner/v4/ActorNode.java
@@ -1,28 +1,54 @@
 package com.vaticle.typedb.core.reasoner.v4;
 
-import com.vaticle.typedb.core.common.exception.TypeDBException;
-import com.vaticle.typedb.core.concept.answer.ConceptMap;
+import com.vaticle.typedb.core.common.iterator.FunctionalIterator;
+import com.vaticle.typedb.core.common.iterator.Iterators;
 import com.vaticle.typedb.core.concurrent.actor.Actor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
-import static com.vaticle.typedb.core.common.exception.ErrorMessage.Internal.UNIMPLEMENTED;
-
 public abstract class ActorNode<NODE extends ActorNode<NODE>>  extends Actor<NODE> {
-
+    protected enum State { READY, PULLING, DONE }
     protected final NodeRegistry nodeRegistry;
+
+    List<ActorNode.Port> ports;
+    private int donePorts;
 
     protected ActorNode(NodeRegistry nodeRegistry, Driver<NODE> driver, Supplier<String> debugName) {
         super(driver, debugName);
         this.nodeRegistry = nodeRegistry;
+        this.ports = new ArrayList<>();
+        this.donePorts = 0;
     }
 
-    public abstract void readAnswerAt(ActorNode<?> reader, int index);
+    public abstract void readAnswerAt(ActorNode.Port reader, int index);
 
-    public abstract void receive(ActorNode<?> sender, Message message);
+    public abstract void receive(ActorNode<NODE>.Port port, Message message);
 
-    public void send(ActorNode<?> recipient, Message message) {
-        recipient.driver().execute(node -> node.receive(this, message));
+    protected Port createPort(ActorNode<?> remote) {
+        Port port = new Port(remote);
+        ports.add(port);
+        return port;
+    }
+
+    protected boolean allPortsDone() {
+        return donePorts == ports.size();
+    }
+
+    protected FunctionalIterator<ActorNode.Port> allPorts() {
+        return Iterators.iterate(ports);
+    }
+
+    // TODO: See if i can safely get recipient from port
+    public void send(ActorNode<?> recipient, ActorNode.Port recipientPort, Message message) {
+        assert recipientPort.remote == this;
+        recipient.driver().execute(actor -> actor.receiveOnPort(recipientPort, message));
+    }
+
+    protected void receiveOnPort(Port port, Message message) {
+        port.recordReceive(message);
+        receive(port, message);
     }
 
     @Override
@@ -35,4 +61,46 @@ public abstract class ActorNode<NODE extends ActorNode<NODE>>  extends Actor<NOD
         super.terminate(e);
     }
 
+
+    public class Port {
+
+        private final ActorNode<?> remote;
+        private State state;
+        private int nextIndex;
+
+        private Port(ActorNode<?> remote) {
+            this.remote = remote;
+            this.state = State.READY;
+            this.nextIndex = 0;
+        }
+
+        public void recordReceive(Message msg) {
+            assert nextIndex == msg.index();
+            nextIndex += 1;
+            if (msg.type() == Message.MessageType.DONE) {
+                state = State.DONE;
+                donePorts += 1;
+            } else {
+                state = State.READY;
+            }
+        }
+
+        public void readNext() {
+            assert state == State.READY;
+            state = State.PULLING;
+            remote.driver().execute(nodeActor -> nodeActor.readAnswerAt(Port.this, nextIndex));
+        }
+
+        public ActorNode<NODE> owner() {
+            return ActorNode.this;
+        }
+
+        public State state() {
+            return state;
+        }
+
+        public int nextIndex() {
+            return nextIndex;
+        }
+    }
 }
