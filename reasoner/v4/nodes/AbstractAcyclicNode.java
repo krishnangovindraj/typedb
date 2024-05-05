b5570ac3ebfba4f6da926414a1c798c239078be1
Krishnan Govindraj <krishnan@vaticle.com>
2024-05-05 15:13:55 +0100

    Better message logging/tracing

--- reasoner/v4/nodes/AbstractAcyclicNode.java
+++ reasoner/v4/nodes/AbstractAcyclicNode.java
@@ -34,11 +34,11 @@ public abstract class AbstractAcyclicNode<NODE extends AbstractAcyclicNode<NODE>
     }
 
     protected void initialise() {
-
+        System.err.printf("CREATION: Node[%d] has been created as a %s\n", this.nodeId, this.getClass().getSimpleName());
     }
 
     public void receiveRequest(ActorNode.Port requester, Request request) {
-        System.err.printf("REQUEST: Node[%d] received response %s from Node[%d]\n", this.nodeId, request, requester.owner().nodeId);
+        System.err.printf("RECV_REQUEST: Node[%d] received request %s from Node[%d]\n", this.nodeId, request, requester.owner().nodeId);
         switch (request.type()) {
             case READ_ANSWER: {
                 readAnswerAt(requester, request.asReadAnswer());
@@ -122,12 +122,13 @@ public abstract class AbstractAcyclicNode<NODE extends AbstractAcyclicNode<NODE>
 
     // TODO: See if i can safely get recipient from port
     protected void sendResponse(ActorNode<?> recipient, ActorNode.Port recipientPort, Response response) { // TODO: Refactor redundant recipient
+        System.err.printf("SEND_RESPONSE: Node[%d] sent response %s to Node[%d]\n", this.nodeId, response, recipient.nodeId);
         assert recipientPort.remote().equals(this);
         recipient.driver().execute(actor -> actor.receiveResponseOnPort(recipientPort, response));
     }
 
     protected void receiveResponseOnPort(ActorNode.Port port, Response response) {
-        System.err.printf("RESPONSE: Node[%d] received response %s from Node[%d]\n", port.owner().nodeId, response, port.remote().nodeId);
+        System.err.printf("RECV_RESPONSE: Node[%d] received response %s from Node[%d]\n", port.owner().nodeId, response, port.remote().nodeId);
         port.recordReceive(response); // Is it strange that I call this implicitly?
         receiveResponse(port, response);
     }
