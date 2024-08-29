39bbb83ac863353a7504f0340e44a683b90a3a2a
Krishnan Govindraj <krishnan@vaticle.com>
2024-08-29 18:26:02 +0530

    Use TypeCache for insert benchmark

--- executor/benches/common.rs
+++ executor/benches/common.rs
@@ -7,6 +7,7 @@
 use std::sync::Arc;
 
 use concept::{thing::thing_manager::ThingManager, type_::type_manager::TypeManager};
+use concept::type_::type_manager::type_cache::TypeCache;
 use durability::wal::WAL;
 use encoding::{
     graph::{
@@ -16,6 +17,7 @@ use encoding::{
     EncodingKeyspace,
 };
 use storage::{durability_client::WALClient, MVCCStorage};
+use storage::sequence_number::SequenceNumber;
 use test_utils::{create_tmp_dir, init_logging, TempDir};
 
 pub fn setup_storage() -> (TempDir, Arc<MVCCStorage<WALClient>>) {
@@ -28,12 +30,17 @@ pub fn setup_storage() -> (TempDir, Arc<MVCCStorage<WALClient>>) {
     (storage_path, storage)
 }
 
-pub fn load_managers(storage: Arc<MVCCStorage<WALClient>>) -> (Arc<TypeManager>, ThingManager) {
+pub fn load_managers(storage: Arc<MVCCStorage<WALClient>>, type_cache_at: Option<SequenceNumber>) -> (Arc<TypeManager>, ThingManager) {
     let definition_key_generator = Arc::new(DefinitionKeyGenerator::new());
     let type_vertex_generator = Arc::new(TypeVertexGenerator::new());
-    let thing_vertex_generator = Arc::new(ThingVertexGenerator::load(storage).unwrap());
+    let thing_vertex_generator = Arc::new(ThingVertexGenerator::load(storage.clone()).unwrap());
+    let cache = if let Some(sequence_number) = type_cache_at {
+        Some(Arc::new(TypeCache::new(storage.clone(), sequence_number).unwrap()))
+    } else {
+        None
+    };
     let type_manager =
-        Arc::new(TypeManager::new(definition_key_generator.clone(), type_vertex_generator.clone(), None));
+        Arc::new(TypeManager::new(definition_key_generator.clone(), type_vertex_generator.clone(), cache));
     let thing_manager = ThingManager::new(thing_vertex_generator.clone(), type_manager.clone());
     (type_manager, thing_manager)
 }
