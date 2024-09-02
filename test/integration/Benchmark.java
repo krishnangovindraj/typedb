e4110be2966e4fe62729aa046fcb164fd255a123
Krishnan Govindraj <krishnan@vaticle.com>
2024-09-02 18:56:56 +0530

    Multithread

--- test/integration/Benchmark.java
+++ test/integration/Benchmark.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 import static com.vaticle.typedb.core.common.collection.Bytes.MB;
 
@@ -22,7 +23,7 @@ public class Benchmark {
     private static final Path logDir = dataDir.resolve("logs");
     private static final Options.Database options = new Options.Database().dataDir(dataDir).reasonerDebuggerDir(logDir)
             .storageIndexCacheSize(MB).storageDataCacheSize(MB);
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Util.resetDirectory(dataDir);
         try (TypeDB.DatabaseManager typedb = CoreDatabaseManager.open(options)) {
             typedb.create(database);
@@ -32,21 +33,45 @@ public class Benchmark {
                     transaction.commit();
                 }
             }
-            long iters = 500_000;
+            int N_THREADS = 4;
+            int N_ITERS = 100_000;
 
             Random random = new Random();
-            long start = System.nanoTime();
-            try (TypeDB.Session session = typedb.session(database, Arguments.Session.Type.DATA)) {
-                for (int i = 0; i < iters; i++) {
-                    try (TypeDB.Transaction transaction = session.transaction(Arguments.Transaction.Type.WRITE)) {
-                        long age = random.nextLong();
-                        transaction.query().insert(TypeQL.parseQuery("insert $x isa person, has age " + age + ";").asInsert());
-                        transaction.commit();
+            Thread[] threads = new Thread[N_THREADS];
+            CountDownLatch signal = new CountDownLatch(1);
+
+            for (int i = 0; i < N_THREADS; i++) {
+                int thread_id = i;
+                threads[thread_id] = new Thread(() -> {
+                    try (TypeDB.Session session = typedb.session(database, Arguments.Session.Type.DATA)) {
+                        try {
+                            signal.await();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        for (int i1 = 0; i1 < N_ITERS; i1++) {
+                            try (TypeDB.Transaction transaction = session.transaction(Arguments.Transaction.Type.WRITE)) {
+                                long age = random.nextLong();
+                                transaction.query().insert(TypeQL.parseQuery("insert $x isa person, has age " + age + ";").asInsert());
+                                transaction.commit();
+                            }
+                        }
                     }
-                }
+                });
             }
-            long elapsed = System.nanoTime() - start;
-            System.out.printf("Total time for %d transactions + iterations: %d (%d nanos per insert) \n", iters, elapsed, elapsed / iters);
+
+            for (int i = 0; i< N_THREADS; i++) {
+                threads[i].start();
+            }
+            long start = System.nanoTime();
+            signal.countDown();
+            long start_took = System.nanoTime() - start;
+            for (int i=0 ; i < N_THREADS; i++) {
+                threads[i].join();
+            }
+            double elapsed_micros = (System.nanoTime() - start)/ 1000.0;
+            System.out.printf("Total time for %d transactions + iterations: %.2f us (%.2f inserts/s) \n", N_ITERS, elapsed_micros, (1000_000.0 * N_ITERS * N_THREADS) / ( elapsed_micros));
+            System.out.printf("Start took: %d nanos", start_took);
         }
     }
 }
